05f1879e3df5cd6f9d1806d5030bf5cfec5a5b39
Dario565 <151281599+Dario565@users.noreply.github.com>
2024-01-04 00:06:59 +0100

    Add files via upload

---
+++ new e-commerce exercise/js/footer.js
@@ -0,0 +1,7 @@
+
+const footerData = {
+name : "Amazon",
+link : "https://www.amazon.it/ref=nav_logo"
+
+     }
+    
---
+++ new e-commerce exercise/js/product.js
@@ -0,0 +1,42 @@
+const products = [
+    {
+        title: "iphone 15",
+        description: "telefono molto costoso",
+        price: 1500,
+        img: "https://m.media-amazon.com/images/I/81izs2I2UvL._AC_UF1000,1000_QL80_.jpg",
+        link:"https://www.amazon.it/Apple-iPhone-15-128-GB/dp/B0CHWV5HTM?th=1"
+    
+    },
+
+    {
+        title: "Motorola Razr",
+        description: "telefono molto costoso",
+        price: 800,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTU0gKHU6uLUcvBBfyQFr1mIHMT-k4hVk1kPA&usqp=CAU",
+        link:"https://www.amazon.it/Motorola-display-flessibile-Qualcomm-Snapdragon/dp/B0BFRN4G8D/ref=sr_1_3?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=298A6P0KGNPPY&keywords=motorola+razr&qid=1704237963&sprefix=motorola+razr%2Caps%2C356&sr=8-3"
+    },
+
+    {
+        title: "Cuffie Beats",
+        description: "Cuffie di ottima qualità",
+        price: 180,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcV1Vwrq9r_NXAjKAHv64JExTHAtQ7O3cPCw&usqp=CAU",
+        link:"https://www.amazon.it/Cuffie-Beats-Solo3-Wireless-Nero/dp/B07YVXGFLS/ref=sr_1_1?__mk_it_IT=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=RNK7UEYCOUTT&keywords=cuffie%2Bbeats&qid=1704238036&sprefix=cuffie%2Bbeats%2Caps%2C111&sr=8-1&th=1"
+    },
+
+    {
+        title: "Alexa echo dot",
+        description: "Assistente vocale",
+        price: 150,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT8mVTZRlyI9ZPxk3fuCggSuXeu3m_COd2XPw&usqp=CAU",
+        link:"https://www.amazon.it/echo-dot-2022/dp/B09B8RF4PY/ref=sr_1_1?crid=1VAHWOBJE380C&keywords=alexa+echo+dot&qid=1704238345&sprefix=alexa+ech%2Caps%2C144&sr=8-1"
+    },
+
+    {
+        title: "PlayStation 5",
+        description: "La miglior console sul commercio",
+        price: 500,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKm3nX1FkknmhuH-A9XcrvkHVWuey4wstqpQ&usqp=CAU",
+        link:"https://www.amazon.it/Playstation-Console-Standard-Modello-Slim/dp/B0CLT54ZNZ/ref=sr_1_1?crid=3P978Q5PVHUGU&keywords=ps5&qid=1704238418&sprefix=ps%2Caps%2C107&sr=8-1"
+    },
+]
